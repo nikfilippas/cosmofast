c493f56906feaaf881f2b53fbfb310a7524276a9
Nick Koukoufilippas <nikfilippas@gmail.com>
2021-03-02 01:29:24 +0200

    works fine

--- test_interp.py
+++ test_interp.py
@@ -19,16 +19,25 @@ import matplotlib.pyplot as plt
 #           "Omega_c": [0.1, 0.9],
 #           "n_s": [0.87, 1.07]}
 
-priors = {"h" : [0.65, 0.75],
-          "sigma8": [0.77, 0.87]}
+priors = {"h" : [0.55, 0.91],
+          "sigma8": [0.50, 1.25],
+          "Omega_b": [0.03, 0.07]}
+
+# priors = {"h" : [0.65, 0.75],
+#           "sigma8": [0.77, 0.87]}
+
 samples = 7**len(priors.keys())
 k_arr = np.logspace(-4, 2, 512)
 a_arr = np.linspace(0.1, 1, 16)
 
-q = interpolator(priors, samples=50,
-                 k_arr=k_arr, a_arr=a_arr,
-                 prefix="rbf-test", overwrite=True,
-                 wpts=16)
+q = interpolator(priors,
+                 samples=500,
+                 k_arr=k_arr,
+                 a_arr=a_arr,
+                 interpf="gaussian",
+                 epsilon=0.1,
+                 prefix="rbf-gauss-3-wide",
+                 overwrite=False)
 
 node = [np.linspace(*priors[par], num=w) for par, w in zip(priors, q.weights)]
 anti = [n[:-1] + np.diff(n)/2 for n in node]
@@ -48,8 +57,7 @@ points = np.vstack(list(map(np.ravel, mgrid))).T
 # k_arr = np.sort(np.append(q.k_arr, k_mid))
 
 kw = interpolator.Planck18()
-d = []
-errs = []
+res = []
 for pnt in points:
     # initiate new cosmology
     kw.update(dict(zip(q.pars, pnt)))
@@ -58,17 +66,18 @@ for pnt in points:
     approx = q.linear_matter_power(cosmo, k_arr, a_arr)
     CAMB = wts.linear_matter_power(cosmo, k_arr, a_arr)
 
-    err = np.fabs(100*(1-approx/CAMB)).max()
-    errs.append(err)
-    d.append(1-approx/CAMB)
-    print(err)
-errs = np.array(errs)
-d = np.array(d)
+    d = 100*(1-approx/CAMB)
+    res.append(d)
+    print(np.fabs(d).max())
 
-d = np.mean(d, axis=0)
+
+
+errs = np.array(np.max(np.fabs(res), axis=(1,2)))
+d = np.mean(np.fabs(res), axis=0)
 
 
 norm = mpl.colors.SymLogNorm(1e-2, base=10)
+plt.figure()
 plt.imshow(d, aspect="auto",
            extent=(np.log10(k_arr[0]),
                    np.log10(k_arr[-1]),
