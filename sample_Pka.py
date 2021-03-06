1a4bfaa5e280009347da35cb057234e8dde1c007
Nick Koukoufilippas <nikfilippas@gmail.com>
2021-03-06 19:32:13 +0200

    Pka sampler, wide priors

---
+++ sample_Pka.py
@@ -0,0 +1,22 @@
+"""Sample P(k,a) with wide cosmological priors."""
+import numpy as np
+from interpolator import interpolator
+
+priors = {"h" : [0.55, 0.91],
+          "sigma8": [0.50, 1.25],
+          "Omega_b": [0.03, 0.07],
+          "Omega_c": [0.1, 0.9],
+          "n_s": [0.87, 1.07]}
+
+## INTERPOLATOR ##
+samples = 16**len(priors.keys())
+k_arr = np.logspace(-4, 2, 512)
+a_arr = np.linspace(0.1, 1, 32)
+
+interp = interpolator(priors,
+                      cosmo_default=None,
+                      k_arr=k_arr,
+                      a_arr=a_arr,
+                      samples=100,
+                      overwrite=False,
+                      just_sample=True)
